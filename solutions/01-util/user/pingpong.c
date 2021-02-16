56f98c4213ada8b3ff2301c08d78da9387c6fd9b
EFanZh <efanzh@gmail.com>
2021-02-17 00:11:46 +0800

    Add CI workflow

--- solutions/01-util/user/pingpong.c
+++ solutions/01-util/user/pingpong.c
@@ -19,25 +19,35 @@ static Pipe create_pipe() {
 int main(int argc, char **argv) {
     const Pipe ping_pipe = create_pipe();
     const Pipe pong_pipe = create_pipe();
-    const int pid = fork();
+    const int fork_pid = fork();
     const int current_pid = getpid();
 
-    if (pid == 0) {
+    if (fork_pid == 0) {
+        close(ping_pipe.write);
+        close(pong_pipe.read);
+
         int buffer;
 
         read(ping_pipe.read, &buffer, 1);
+        close(ping_pipe.read);
 
         printf("%d: received ping\n", current_pid);
 
         write(pong_pipe.write, &buffer, 1);
+        close(pong_pipe.write);
     } else {
+        close(ping_pipe.read);
+        close(pong_pipe.write);
+
         const unsigned char data = 7;
 
         write(ping_pipe.write, &data, 1);
+        close(ping_pipe.write);
 
         int buffer;
 
         read(pong_pipe.read, &buffer, 1);
+        close(pong_pipe.read);
 
         printf("%d: received pong\n", current_pid);
 
